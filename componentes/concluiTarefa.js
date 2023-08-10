e0042d297a9affd2e6b94e3c415c2f4951675939
ALLYSSON CIDADE <120651526+AllyssonCidade@users.noreply.github.com>
2023-08-10 14:49:01 -0300

    Update concluiTarefa.js

--- componentes/concluiTarefa.js
+++ componentes/concluiTarefa.js
@@ -1,18 +1,33 @@
 const BotaoConclui = () => {
-    const botaoConclui = document.createElement('button')
+    const botaoConclui = document.createElement('button');
 
-    botaoConclui.classList.add('check-button')
-    botaoConclui.innerText = 'concluir'
-    botaoConclui.addEventListener('click', concluirTarefa)
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = 'concluir';
+    botaoConclui.addEventListener('click', concluirTarefa);
 
-    return botaoConclui
-}
-const concluirTarefa = (evento) =>{
-    const botaoConclui = evento.target
+    return botaoConclui;
+};
 
-    const tarefaCompleta = botaoConclui.parentElement
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target;
 
-    tarefaCompleta.classList.toggle('done')
-}
+    const tarefaCompleta = botaoConclui.parentElement;
+    tarefaCompleta.classList.toggle('done'); // Alterna a classe "done" no DOM
 
-export default BotaoConclui
+    const valorTarefa = tarefaCompleta.querySelector('.content').textContent;
+    atualizarClasseTarefaLocalStorage(valorTarefa, tarefaCompleta.classList.contains('done'));
+};
+
+const atualizarClasseTarefaLocalStorage = (valorTarefa, concluida) => {
+    const tarefasSalvas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+    const tarefaEncontrada = tarefasSalvas.find(tarefa => tarefa.valor === valorTarefa);
+
+    if (tarefaEncontrada) {
+        tarefaEncontrada.clase = concluida ? 'task done' : 'task';
+
+        localStorage.setItem('tarefas', JSON.stringify(tarefasSalvas));
+    }
+};
+
+export default BotaoConclui;
